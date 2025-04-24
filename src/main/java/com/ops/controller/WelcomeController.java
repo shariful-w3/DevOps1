0563c5d478ed75b36acb90f2bb6bd1bfc044f53f
SharifulW3 <shariful.w3@gmail.com>
2025-04-24 11:41:17 +0600

    updated main contr update

--- src/main/java/com/ops/controller/WelcomeController.java
+++ src/main/java/com/ops/controller/WelcomeController.java
@@ -12,7 +12,7 @@ public class WelcomeController {
 
 		model.addAttribute("title", "Title: DevOps Service");
 		model.addAttribute("pageTitle", "Page Title: DevOps Service");
-		model.addAttribute("msg", "Msg: Welcome To DepOps CI/CD pipeline demo, Commit 666666666y!");
+		model.addAttribute("msg", "Msg: Welcome To DepOps CI/CD pipeline demo, Commit shanto bhai is here");
 
 		model.addAttribute("msg2", "Msg2: Welcome To DepOps CI/CD pipeline demo!");
 
